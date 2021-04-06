418c5347d0888fbe136ad5b8ee2e048b4a5f81be
chrissvega14 <82060999+chrissvega14@users.noreply.github.com>
2021-04-06 16:41:14 -0500

    .

--- app/src/main/java/com/example/howifeel/Inicio.java
+++ app/src/main/java/com/example/howifeel/Inicio.java
@@ -11,8 +11,6 @@ import com.google.firebase.auth.FirebaseAuth;
 public class Inicio extends AppCompatActivity {
 
     @Override
-
-    //prueba github
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
